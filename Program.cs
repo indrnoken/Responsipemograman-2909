d7c528395314977cffa1711a98cee7b3525b6b8b
indrnoken <61928913+indrnoken@users.noreply.github.com>
2020-04-15 15:34:44 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemograman2909
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            karyawan im = new karyawan(1, 082238, "acer", 20000);
+            karyawan him = new karyawan(2, 39898474, "sam", 2800000);
+
+            Console.WriteLine("no \anik \a \a nama \a gaji bulanan");
+            im.data();
+            him.data();
+
+
+
+            Console.WriteLine("no \anik \a \a nama \a gaji bulanan");
+            im.naikgaji();
+            him.naikgaji();
+
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
---
+++ karyawan.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemograman2909
+{
+    class karyawan
+    {
+        public string Nama { get; set; }
+        public int gajibulanan { get; set; }
+        public int nik { get; set; }
+        public int nomor { get; set; }
+
+        public karyawan(int nomor, int nik, int gajibulanan, string nama)
+        {
+            this.nomor = nomor;
+            this.nik = nik;
+            this.Nama = nama;
+
+            if (gajibulanan < 0)
+            {
+                this.gajibulanan = 0;
+            }
+            else
+            {
+                this.gajibulanan = gajibulanan;
+
+            }
+
+
+        }
+
+        public void data()
+        {
+            Console.WriteLine("{1}\a{11}\a {2}\a{3}", nomor, nik, Nama, gajibulanan);
+
+        }
+
+        public void naikgaji()
+        {
+            double tmp = 0;
+            tmp = 0.1 * gajibulanan;
+            gajibulanan += Convert.ToInt32(tmp);
+            Console.WriteLine("{1}\a{11}\a {2}\a{3}", nomor, nik, Nama, gajibulanan);
+        }
+
+    }
+
+}
+
+
